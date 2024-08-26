6e20fb45a12db399a11ab1a240c62f8dd66a0cf6
ARTHUR VIECILI PINHEIRO DA SILVA <arthur_vp_silva@estudante.sesisenai.org.br>
2024-08-26 16:45:08 -0300

    new commit

---
+++ codes/project-pharma/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Body from './assets/components/body'
+import Header from './assets/components/header'
+
+function App() {
+
+  return (
+   <div className='app-container'>
+
+      <Header/>
+
+      <Body/>
+
+   </div>
+  
+  )
+}
+
+export default App
---
+++ codes/project-pharma/src/assets/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+
+        <div className='logotipo'>
+            <img className='img-logotipo' src="../public/images/farmarcia-logo-pq.png"/>
+        </div>
+
+        <div className='titulo'>
+
+            <label className='titulo-farmacia'>Farmárcia</label>
+
+        </div>
+
+        <div className='vazio'>
+
+        </div>
+
+        <div className='sections'>
+            <label className='loja'>Loja</label>
+            <label className='local'>Local</label>
+            <label className='quem-somos'>quem somos</label>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
